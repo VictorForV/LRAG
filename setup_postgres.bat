9be7304b71674bab3557535eeaefdc0162581056
VictorForV <victorforv4785@gmail.com>
2026-02-10 19:19:59 +0300

    fix: Remove Cyrillic from setup_postgres.bat to avoid encoding errors

--- setup_postgres.bat
+++ setup_postgres.bat
@@ -1,35 +1,34 @@
 @echo off
-chcp 65001 >nul 2>&1
+setlocal enabledelayedexpansion
 
 echo ==================================================
-echo   PostgreSQL Portable - Setup
+echo   PostgreSQL Portable Setup
 echo ==================================================
-echo.
-echo PostgreSQL budet ustanovlen v papku postgres/
 echo.
 
 if exist postgres (
-    echo [X] Papka postgres uzhe sushchestvuet
+    echo [X] Postgres folder already exists
+    echo     Delete it first if you want to reinstall
     pause
     exit /b 0
 )
 
-echo [*] Sozdanie portable PostgreSQL...
-echo     Eto znymet neskolko minut...
+echo [*] Creating portable PostgreSQL...
+echo     This will take a few minutes...
 echo.
 
 mkdir postgres\data
 
-echo [1/3] Skachivanie PostgreSQL...
-powershell -Command "Invoke-WebRequest -Uri 'https://sbp.enterprisedb.com/getfile.jsp?fileid=1259243' -OutFile 'pg_binaries.zip' -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome"
+echo [1/3] Downloading PostgreSQL...
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://sbp.enterprisedb.com/getfile.jsp?fileid=1259243' -OutFile 'pg_binaries.zip' -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome"
 
 if not exist pg_binaries.zip (
-    echo [X] Oshibka skachivaniya!
+    echo [X] Download failed!
     pause
     exit /b 1
 )
 
-echo [2/3] Raspakovka...
+echo [2/3] Extracting...
 powershell -Command "Expand-Archive -Path 'pg_binaries.zip' -DestinationPath 'postgres' -Force"
 del pg_binaries.zip
 
@@ -39,15 +38,15 @@ if exist postgres\bin\psql.exe set "PG_BIN=postgres\bin"
 for /d %%i in (postgres\postgresql-*) do if not defined PG_BIN set "PG_BIN=%%i\bin"
 
 if not exist "!PG_BIN!\psql.exe" (
-    echo [X] Ne nayden PostgreSQL bin!
+    echo [X] PostgreSQL bin not found!
     pause
     exit /b 1
 )
 
-echo [3/3] Inicializaciya BD...
+echo [3/3] Initializing database...
 "!PG_BIN!\initdb.exe" -D postgres\data -U postgres -A trust -E utf8 --locale=C
 
 echo.
-echo [OK] PostgreSQL gotov!
+echo [OK] PostgreSQL ready!
 echo.
 pause
